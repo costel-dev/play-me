a3c6fd69c7d063710c902bb350a56886b9f07f5b
costel-dev <costelcuconoiu@gmail.com>
2021-01-25 22:29:17 +0100

    Navbar component created and added to our project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <img src="../assets/playme.png" alt="Play Me Logo" />
+      <h1><router-link :to="{ name: 'Home' }">PlayMe</router-link></h1>
+      <div class="links">
+        <button>Logout</button>
+        <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
+        <router-link class="btn" :to="{ name: 'Login' }">Login</router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style scoped>
+.navbar {
+  padding: 16px 10px;
+  margin-bottom: 60px;
+  background: white;
+}
+nav {
+  display: flex;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+nav h1 {
+  margin-left: 20px;
+}
+nav .links {
+  margin-left: auto;
+}
+nav .links a,
+button {
+  margin-left: 16px;
+  font-size: 14px;
+}
+nav img {
+  max-height: 60px;
+}
+</style>
