c5b02ee2b34fbfd79a61d640c15b5ad5e0537303
costel-dev <costelcuconoiu@gmail.com>
2021-01-25 21:31:17 +0100

    added Signup view component and succesfully created and account in ur firebase storage

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Login from '../views/Login.auth.vue';
+import Signup from '../views/Signup.auth.vue';
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login,
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup,
   },
 ];
 
